17832dbd23e788345080c54df7ea9f29fb9e3a1d
Cathy <smile25896@gmail.com>
2021-09-19 10:26:36 +0800

    題目

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ let cardDom = document.querySelector(".card");
 let questionDom = document.querySelector(".question");
 
 cardDom.addEventListener("click", changeQuestion);
+let index = 0;
 
 function changeQuestion() {
+  let data = dataString.split(/\s+/);
+  if (index >= data.length - 1) {
+    questionDom.textContent = "題目用完了";
+    return;
+  }
   if (cardDom.classList.contains("unstart")) {
     cardDom.classList.remove("unstart");
   }
-  questionDom.textContent = data[getRandomArbitrary(0, data.length - 1)];
+  sort(data);
+  questionDom.textContent = data[index++];
+  // questionDom.textContent = data[getRandomArbitrary(0, data.length - 1)];
 }
 
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function sort(array) {
+  array.sort(() => Math.random() - 0.5);
 }
